a8a1993d57f44196df50766185597350e3489c10
Renato Xavier <rnatu91@gmail.com>
2021-05-31 15:05:54 -0300

    feat: add initial functions of Infinite Queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,14 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    async ({ pageParam = 4 }) => {
+      const response = await api.get(`/api/images?after=${pageParam}`);
+      return response.data;
+    },
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => lastPage.after || null,
+    }
   );
 
   const formattedData = useMemo(() => {
@@ -36,7 +42,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
